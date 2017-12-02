26ac1932ae40d9ee19bcba70ce7ce4e79f880f7e
Abfahrt <abfahrt.qy@gmail.com>
2017-12-02 20:36:38 +0800

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,7 @@ import unittest
 class NewVisitTest(unittest.TestCase):
     def setUp(self):
         self.browser=webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        #self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
