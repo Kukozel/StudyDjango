c62a11b879806f9edf81f24622a933e6ee7464a5
Abfahrt <abfahrt.qy@gmail.com>
2017-12-04 21:35:07 +0800

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -27,11 +27,35 @@ class NewVisitTest(LiveServerTestCase):
                          'Enter a to-do item'
                          )
         inputbox.send_keys('Buy peacock feathers')
-
         inputbox.send_keys(Keys.ENTER)
+
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
+
         import time
         time.sleep(3)
 
         self.check_for_row_in_list_table('1:Buy peacock feathers')
+
+        self.browser.quit()
+        self.browser=webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url=self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(edith_list_url,francis_list_url)
+
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertIn('Buy milk',page_text)
+
+
 
         self.fail('Finish a test!')
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from lists.views import home_page
+from lists.views import view_list
 
 urlpatterns = [
-    url(r'^$',include('lists.urls'),name='home'),
+    url(r'^$', home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', view_list, name='view_list'),
     url(r'^admin/', admin.site.urls),
 ]
