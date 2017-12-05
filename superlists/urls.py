714642b330f55d06b0432a2938d12b0f435a10ee
Abfahrt <abfahrt.qy@gmail.com>
2017-12-05 16:12:34 +0800

    optimize URLs

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,14 +16,10 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from lists.views import home_page
-from lists.views import view_list
-from lists.views import new_list
-from lists.views import add_item
+
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
-    url(r'^lists/(\d+)/$', view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', add_item, name='add_item'),
-    url(r'^lists/new$', new_list, name='new_list'),
+    url(r'^lists/', include('lists.urls')),
     #url(r'^admin/', admin.site.urls),
 ]
