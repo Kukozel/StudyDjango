d5fdb26cf80bb587dfcd5665229938b7f461da31
Abfahrt <abfahrt.qy@gmail.com>
2017-12-25 01:33:47 +0800

    use form in all views,back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,7 +7,7 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text,"You can't have an empty list item")
 
