6b7a4eb74db57de868fe47a0d3a1324e1af01352
Abfahrt <abfahrt.qy@gmail.com>
2017-12-25 00:05:48 +0800

    use new form in home_page,simplify tests. NB break stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,29 +7,6 @@ from lists.forms import ItemForm
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_url_resolve_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func,home_page)
-
-    # def test_home_page_returns_correct_html(self):
-    #     request=HttpRequest()
-    #     response=home_page(request)
-    #     self.assertIn('A new list item',response.content.decode())
-    #     expected_html = render_to_string('home.html',
-    #                                      {'new_item_text':'A new list item'}
-    #                                      )
-    #     self.assertEqual(response.content.decode(),expected_html)
-
-
-    # def test_home_page_displays_all_list_items(self):
-    #     Item.objects.create(text='itemey 1')
-    #     Item.objects.create(text='itemey 2')
-    #
-    #     request=HttpRequest()
-    #     response=home_page(request)
-    #
-    #     self.assertIn('itemey 1',response.content.decode())
-    #     self.assertIn('itemey 2',response.content.decode())
 
     def test_home_page_renders_home_template(self):
         response=self.client.get('/')
