b8770dadfc343426631b21e7769fd2ec746426a3
Abfahrt <abfahrt.qy@gmail.com>
2017-12-24 22:18:11 +0800

    correct some error at function test about time.sleep()

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,26 +1,32 @@
 from .base import FunctionalTest
 from unittest import skip
+from selenium.webdriver.common.keys import Keys
+import time
 
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\r')
-
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        time.sleep(1)
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text,"You can't have an empty list item")
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\r')
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        time.sleep(1)
         self.check_for_row_in_list_table('1:Buy milk')
 
-        self.browser.find_element_by_id('id_new_item').send_keys('\r')
-
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        time.sleep(10)
         self.check_for_row_in_list_table('1:Buy milk')
         error = self.browser.find_element_by_css_selector('.has-error')
         self.assertEqual(error.text, "You can't have an empty list item")
 
-        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\r')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        time.sleep(1)
         self.check_for_row_in_list_table('1:Buy milk')
-        self.check_for_row_in_list_table('1:Make tea')
+        self.check_for_row_in_list_table('2:Make tea')
 
 
         #self.fail('Write Me!')
