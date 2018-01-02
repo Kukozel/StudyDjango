77eb9cf4a76e1017e2c8f5aa56e71fad87226317
Abfahrt <abfahrt.qy@gmail.com>
2018-01-02 16:30:56 +0800

    remove my account

--- viewInfo/mongodbInfo.py
+++
@@ -1,7 +0,0 @@
-class account(object):
-    def __init__(self):
-        self.db='qzrc'
-        self.user='abfahrt'
-        self.password='abfahrt'
-        self.collection='infos'
-        self.host='139.199.159.233'
